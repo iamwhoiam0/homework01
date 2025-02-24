c7f14ed94f923511b4c92d9e6ffd5fd8f91ed5d2
iamwhoiam0 <gamzatgasanov2337@mail.ru>
2025-02-24 19:46:03 +0300

    done

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,5 +2,18 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+    val numbers = Array(n) { "" }
+    for (i in 0..<n){
+        if ((i%3 == 0) and (i%5==0)){
+            numbers[i] = "FizzBuzz"
+        }
+        else if (i%3==0){
+            numbers[i] = "Fizz"
+        }else if(i%5==0){
+            numbers[i] = "Buzz"
+        }else {
+            numbers[i] = i.toString()
+        }
+    }
+    return numbers
 }
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -2,5 +2,24 @@ package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+    val result = mutableListOf<Int>()
+    val indexMap = mutableMapOf<Int, MutableList<Int>>()
+
+    for (i in numbers.indices) {
+        val complement = target - numbers[i]
+
+        indexMap[complement]?.forEach { j ->
+            result.add(j)
+            result.add(i)
+        }
+
+        indexMap.getOrPut(numbers[i]) { mutableListOf() }.add(i)
+    }
+
+    // Проверка на наличие пар
+    require(result.isNotEmpty()) {
+        "Не найдено пар с суммой $target в массиве ${numbers.contentToString()}"
+    }
+
+    return result.toIntArray()
 }
